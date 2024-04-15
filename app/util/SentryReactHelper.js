81f7d8cfeb90a56a07f604e30c6e18178b7fa57a
Leonidas Conde <80922146+LeonardoDizConde@users.noreply.github.com>
2024-04-15 19:22:09 -0300

    CU-86dt8mzpz - NEON2 - Sentry - Remove IP Collection

--- app/util/SentryReactHelper.js
+++ app/util/SentryReactHelper.js
@@ -9,14 +9,20 @@ export function setupSentryReact() {
         'https://84c269b6f724a4ec47e140b42f4e6d7d@o1193870.ingest.us.sentry.io/4506502883639296',
       environment: 'NEON-2',
       attachStacktrace: true,
-      autoSessionTracking: true,
+      autoSessionTracking: false,
       debug: false,
-      enableTracing: true,
+      enableTracing: false,
       replaysOnErrorSampleRate: 1.0,
       replaysSessionSampleRate: 0.1,
       tracePropagationTargets: ['localhost'],
       tracesSampleRate: 1.0,
       version: process.env.npm_package_version,
+      sendDefaultPii: false,
+      beforeSend(event) {
+        if (event.request.url) delete event.request.url
+        if (event.user.ip_address) delete event.user.ip_address
+        return event
+      },
     })
   }
 }
